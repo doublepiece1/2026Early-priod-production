12475c16776056d838eb2178247336d38d741b4c
shiina04jbs10 <shiina04jbs10@gmail.com>
2026-05-08 18:54:27 +0900

    シーン遷移ボタン追加

--- Assets/Script/TitleScene/TitleStartButton.cs
+++ Assets/Script/TitleScene/TitleStartButton.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Kounosuke
 {
     public class TitleStartButton : TitleButtonBase
     {
+        [SerializeField, Header("遷移するシーン名")] private string Scene_Name;
         protected override void OnClickSettingButton()
         {
             base.OnClickSettingButton();
             Debug.Log("スタートボタンが押されました");
+            if (Scene_Name != null) { 
+                SceneManager.LoadScene(Scene_Name);
+            }
         }
     }
 }
