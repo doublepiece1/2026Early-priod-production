e4d855965e286ad6f004e67a628b3c3be937d3fd
uiuishiina <shiina04jbs10@gmail.com>
2026-06-01 17:58:36 +0900

    Fade追加

--- Assets/Unity.VisualScripting.Generated/TarzanAction.cs
+++ Assets/Unity.VisualScripting.Generated/TarzanAction.cs
@@ -90,9 +90,9 @@ public class TarzanAction : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Trap"))
-        {
-            Die();
-        }
+        //if (collision.gameObject.CompareTag("Trap"))
+        //{
+        //    Die();
+        //}
     }
 }
