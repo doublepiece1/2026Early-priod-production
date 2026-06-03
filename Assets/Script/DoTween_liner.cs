d9f1e471a34269920551b648661f5363c6de795b
うい 椎名 <shiina04jbs10@outlook.com>
2026-06-03 12:29:35 +0900

    移動床完成、まだループするだけなのでプレイヤー判定時に移動開始のするものも用意する予定

--- Assets/Script/DoTween_liner.cs
+++ Assets/Script/DoTween_liner.cs
@@ -5,15 +5,16 @@ namespace Kounosuke
 {
     public class DoTween_liner : MonoBehaviour
     {
-        [SerializeField, Header("")] private Vector3 move_vec = new Vector3();
-        [SerializeField, Header("")] private float time_ = 1;
-        [SerializeField, Header("")] private int loop_count = -1;
-        [SerializeField, Header("")] private LoopType loop_mode = LoopType.Yoyo;
+        [SerializeField, Header("移動するベクトル")] private Vector3 move_vec = new Vector3();
+        [SerializeField, Header("一回の移動時間")] private float time_ = 1;
+        [SerializeField, Header("ループ回数")] private int loop_count = -1;
+        [SerializeField, Header("ループモード")] private LoopType loop_mode = LoopType.Yoyo;
+        [SerializeField, Header("移動方法")] private Ease ease_mode = Ease.Linear;
         Vector3 start_pos;
         private void Start()
         {
             start_pos = transform.position;
-            this.transform.DOMove(start_pos + move_vec, time_).SetLoops(loop_count, loop_mode);
+            this.transform.DOMove(start_pos + move_vec, time_).SetLoops(loop_count, loop_mode).SetEase(Ease.Linear);
         }
     }
 }
